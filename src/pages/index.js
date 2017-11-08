478a7031d5e2f5c1044199600bef72c4b34458ce
Brandon Petty <brandonpetty@gmail.com>
2017-11-08 00:35:18 -0500

    Fix small typo on intro-card

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div className="home">
     <div className="intro-card">
-      <h1>Hello! I'm Brandon Petty, a full stack developer living in Altanta, Georgia with over 10 years industry experience.</h1>
-      <p>I currently work at <a href="https://www.hirezstudios.com/">Hi-Rez Studios</a> - maker of <a href="https://www.smitegame.com/">Smite</a>, <a href="https://www.paladins.com/">Paladins</a>, and several other free-to-play games. <br /> My previous work includes engineering at <a href="https://www.amazon.com/">Amazon</a> and <a href="https://www.airforce.com/">The United States Air Force</a>.</p>
+      <h1>Hello! I'm Brandon Petty, a full stack developer living in Atlanta, Georgia with over 10 years industry experience.</h1>
+      <p>I currently work at <a href="https://www.hirezstudios.com/">Hi-Rez Studios</a> - makers of <a href="https://www.smitegame.com/">Smite</a>, <a href="https://www.paladins.com/">Paladins</a>, and several other free-to-play games. <br /> My previous work includes engineering at <a href="https://www.amazon.com/">Amazon</a> and <a href="https://www.airforce.com/">The United States Air Force</a>.</p>
     </div>
     <div className="read">
       <h1>2017</h1>
